b12aadd9e1a366186c0edb392c2ee6b1321a39ab
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-09 20:59:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgContainer =document.getElementById("dog-image-container");
+document.addEventListener("DOMContentLoaded", function() {
+  fetchImgUrl()
+})
+
+function fetchImgUrl()
+{
+  fetch(imgUrl)
+  .then(response => response.json())
+  .then(data => somImages(data))
+}
+
+function somImages(data) {
+    data.message.forEach(image => {
+    const img = document.createElement('img')
+    img.src = image;
+    img.style.width="500px";
+    imgContainer.appendChild(img)
+    })
+  }
+  
