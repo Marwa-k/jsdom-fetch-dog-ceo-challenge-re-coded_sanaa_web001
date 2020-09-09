c68ba77d8b3cefb4573cdb7c291cb57f6a6ec529
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-09 21:08:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ function somBreeds(response){
         }
         
          let li = document.querySelectorAll('li');
-          li.forEach(eachLi);
+          li.forEach(liColor);
         function colorChange(event){
             event.target.style.color = 'blue';
         }
